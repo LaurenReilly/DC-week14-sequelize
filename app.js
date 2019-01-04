cd50e17aee80a0ad980add385dbe439ce0186823
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-01-03 21:03:26 -0500

    completed assignment

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 const express = require('express');
 const app = express();
 
+const prompt = require('prompt-promise');
+
 const models = require('./models');
 
-models.user.create({firstName: 'Lauren', lastName: 'Reilly', email: 'heygirl@gmail.com'});
+let newUser = {firstName: '', lastName: '', email: ''};
 
-app.get('/', function(req, res){
-    models.user.findAll()
-    .then((results) => {
-        results.forEach(function(index){
-            console.log(index.id, index.firstName);
+// models.user.create({firstName: 'Lauren', lastName: 'Reilly', email: 'heygirl@gmail.com'});
+
+// app.get('/', function(req, res){
+//     models.user.findAll()
+//     .then((results) => {
+//         results.forEach(function(index){
+//             console.log(index.id, index.firstName);
+//         });
+//     });
+// });
+
+function addUser(){ 
+    prompt('First Name: ')
+    .then(function (val) {
+    newUser.firstName = val;
+    prompt('Last Name: ')
+    .then(function(val){
+        newUser.lastName = val;
+        prompt('Email: ')
+        .then(function(val){
+            newUser.email = val;
+            models.user.create(newUser)
+            models.user.findOne({where: {lastName: newUser.lastName}})
+            .then((results) => {
+                var r = results.dataValues
+                console.log('Created User with Id of: ', r.id);
+                prompt('Enter Another User? Enter Y/N\n')
+                .then(function(val){
+                    var answer = val;
+                    if (answer == "y" || answer == "Y") {
+                        addAlbum();
+                    } else {
+                        console.log("Ok...goodbye then")
+                        prompt.done();
+                    }
+                });
+            });
         });
     });
-});
+})
+    .catch(function rejected(err) {
+    console.log('error:', err.stack);
+    pgp.end();
+    prompt.finish();
+    });
+}
+
+addUser();
+
+
 
 app.listen(3000, function(){
-    console.log('listening on port 3000');
 });
